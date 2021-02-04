1577535b88c10745e24ce43f586077d4a512d93f
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-04 14:23:26 +0600

    add let

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let html = document.documentElement;
+let head = document.head;
+let body = document.body;
+
+let header = document.querySelector('h1');
+let article = document.querySelector('article');
+let p = document.querySelectorAll('p');
